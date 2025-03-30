f46ba48258281cf8307114a236a6c7439da4872d
GabrielCornejoB <gabrielcorbot@gmail.com>
2025-03-30 12:35:29 -0500

    feat(ms-auth): :sparkles: create adapter that overrides spring security user details service

---
+++ ms-auth/infrastructure/driven-adapters/jwt/src/main/java/co/com/bancolombia/jwt/UserDetailsAdapter.java
@@ -0,0 +1,31 @@
+package co.com.bancolombia.jwt;
+
+import co.com.bancolombia.model.authentication.UserModel;
+import co.com.bancolombia.model.authentication.gateways.AuthenticationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsAdapter implements UserDetailsService {
+
+    private AuthenticationRepository authenticationRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = this.authenticationRepository.findByCc(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("No se encontró al usuario con cc: " + username);
+        }
+
+        return new User(user.getCc(), user.getPassword(), Collections.emptyList());
+    }
+
+}
